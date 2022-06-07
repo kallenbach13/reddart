f4340aee24e55c7434c2708f14b92c6c73ab569e
Isak Kallenbach <kallenbach13@gmail.com>
2022-06-07 17:59:46 +0100

    all the initial file setup complete and the header component implemented

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="nav-bar">    
+      <h2 id="title">Little Reddit</h2>
+    </div>
+  );
+}
+
+export default Header;
