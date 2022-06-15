5d50d9b816e9de30f3a0529781e80f68de8cbc32
Isak Kallenbach <kallenbach13@gmail.com>
2022-06-15 15:31:58 +0100

    aligned items correctly in subreddit container and added some css to the buttons within

--- src/SubredditContainer.js
+++ src/SubredditContainer.js
@@ -2,40 +2,41 @@ import React from 'react';
 
 const SubredditContainer = (props) => {
     return (
-        <div id="subreddits-container" class="sticky-top">    
-            <h2 id="subreddits-list-title">Subreddits</h2>
+        <div id="subreddits-container" class="sticky-top">
 
-            <ul id="subreddits-list">
+            <h2 id="subreddits-title">Subreddits</h2>
 
-                <button className='btn' name='Art' value='art' onClick={e => props.setSubreddit(e.target.value)}> 
+            <div id="subreddits-list">
+
+                <button className='btn subreddit-button' id="art-button" name='art' value='art' onClick={e => props.setSubreddit(e.target.value)}> 
                     Art
                 </button>
 
-                <button className='btn' name='Pixel-Art' value='pixelart' onClick={e => props.setSubreddit(e.target.value)}> 
+                <button className='btn subreddit-button' id="pixelart-button" name='pixelart' value='pixelart' onClick={e => props.setSubreddit(e.target.value)}> 
                     Pixel Art
                 </button>
 
-                <button className='btn' name='Watercolor' value='watercolor' onClick={e => props.setSubreddit(e.target.value)}> 
+                <button className='btn subreddit-button' id="watercolor-button" name='watercolor' value='watercolor' onClick={e => props.setSubreddit(e.target.value)}> 
                     Watercolor
                 </button>
 
-                <button className='btn' name='Analog' value='analog' onClick={e => props.setSubreddit(e.target.value)}> 
+                <button className='btn subreddit-button' id="analog-button" name='analog' value='analog' onClick={e => props.setSubreddit(e.target.value)}> 
                     Analog
                 </button>
 
-                <button className='btn' name='Graffiti' value='graffiti' onClick={e => props.setSubreddit(e.target.value)}> 
+                <button className='btn subreddit-button' id="graffiti-button" name='graffiti' value='graffiti' onClick={e => props.setSubreddit(e.target.value)}> 
                     Graffiti
                 </button>
 
-                <button className='btn' name='Sculpture' value='sculpture' onClick={e => props.setSubreddit(e.target.value)}> 
+                <button className='btn subreddit-button' id="sculpture-button" name='scultpture' value='sculpture' onClick={e => props.setSubreddit(e.target.value)}> 
                     Sculpture
                 </button>
 
-                <button className='btn' name='Oil-Painting' value='oilpainting' onClick={e => props.setSubreddit(e.target.value)}> 
+                <button className='btn subreddit-button' id="oilpainting-button" name='oilpainting' value='oilpainting' onClick={e => props.setSubreddit(e.target.value)}> 
                     Oil Painting
                 </button>
 
-        </ul>
+        </div>
 
       </div>
     )
