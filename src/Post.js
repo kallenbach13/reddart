83b83f68f0732d80699f9e06c591def1248078f9
Isak Kallenbach <kallenbach13@gmail.com>
2022-06-15 14:49:48 +0100

    giving classnames to post text and changing their sizing

--- src/Post.js
+++ src/Post.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const shortenSelfText = (text, limit) => {
+const shortenText = (text, limit) => {
 	const short = text.indexOf(' ', limit);
 	if(short === -1) return text;
-	return text.substring(0, short);
+	return text.substring(0, short) + "...";
 }
 
 // Might add a shortenTitle function as well in the future. Some of the post titles on Reddit can get a bit longwinded.
@@ -16,13 +16,13 @@ function Post(props) {
 	return(
 
 		<div className="col-sm-3 post-card" onClick={()=> window.open("https://reddit.com" + props.post.permalink, "_blank")}>
-			<h3>{props.post.title}</h3>
-			<p>Posted by {props.post.author}</p>
+			<h3 className="post-title">{shortenText(props.post.title, 30)}</h3>
+			<p className="author-text">Posted by {props.post.author}</p>
 			<img src={image} className="image-item" alt=" "/>
-			<p>{shortenSelfText(props.post.selftext, 100)}</p>
+			<p>{shortenText(props.post.selftext, 100)}</p>
 			<div id="comments-and-upvotes-container">
-				<p>Comments: {props.post.num_comments}</p>
-				<p>Upvotes: {props.post.score}</p>
+				<p className="comments-and-score-text">Comments: {props.post.num_comments}</p>
+				<p className="comments-and-score-text">Upvotes: {props.post.score}</p>
 			</div>
 			
 		</div>
